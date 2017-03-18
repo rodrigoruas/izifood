a7197aa42c9918f4295885858deeef52c1086135
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-03-18 20:32:10 -0300

    home melhorada

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
-  root to: "devise/sessions#new"
+  root to: "pages#home"
   end
 
 end
